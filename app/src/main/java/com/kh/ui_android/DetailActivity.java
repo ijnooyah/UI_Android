f1d1307cb40dd81952e3b278a390ee555cc46008
ijnooyah <goeun0918s2@gmail.com>
2021-06-08 02:21:24 +0900

    selectbysno 수정
    요구사항이 학번으로 검색이 아니라 이름으로 검색이었음 그래서 수정함 그리고 이름 like % % 이용해서 하게 수정함

--- app/src/main/java/com/kh/ui_android/DetailActivity.java
+++ app/src/main/java/com/kh/ui_android/DetailActivity.java
@@ -42,7 +42,7 @@ public class DetailActivity extends AppCompatActivity {
         lvDetail = findViewById(R.id.lvDetail);
         helper = new MyDBHelper(getApplicationContext(), "StudentDB", null, 1);
 
-        String[] data = {"학번", "전공"};
+        String[] data = {"이름", "전공"};
         sAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
         spinnerDetail.setAdapter(sAdapter);
 
@@ -50,17 +50,18 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String slectedItem = spinnerDetail.getSelectedItem().toString();
-                if (slectedItem.equals("학번")) {
+                if (slectedItem.equals("이름")) {
                     Log.d("mytag", "학번선택");
                     list = new ArrayList<>();
-                    String sno = edtDetail.getText().toString();
+                    String name = edtDetail.getText().toString();
 //            Log.d("mytag", "selectAll");
                     SQLiteDatabase db = helper.getReadableDatabase();
                     String sql = "select * from tbl_student" +
-                            "     where sno = " + sno +
+                            "     where sname like '%" + name + "%'" +
                             "     order by sno";
                     Cursor cursor = db.rawQuery(sql, null);
                     while (cursor.moveToNext()) {
+                        String sno = cursor.getString(0);
                         String sname = cursor.getString(1);
                         int syear = cursor.getInt(2);
                         String gender = cursor.getString(3);
@@ -75,19 +76,20 @@ public class DetailActivity extends AppCompatActivity {
                 } else if (slectedItem.equals("전공")) {
                     Log.d("mytag", "전공선택");
                     list = new ArrayList<>();
-                    String major = edtDetail.getText().toString().trim();
+                    String smajor = edtDetail.getText().toString().trim();
 //            Log.d("mytag", "selectAll");
                     SQLiteDatabase db = helper.getReadableDatabase();
                     String sql = "select * from tbl_student" +
-                            "     where major = ?" +
+                            "     where major like '%" + smajor + "%'" +
                             "     order by sno";
-                    Cursor cursor = db.rawQuery(sql, new String[]{major});
+                    Cursor cursor = db.rawQuery(sql, null);
                     Log.d("mytag", cursor.toString());
                     while (cursor.moveToNext()) {
                         String sno = cursor.getString(0);
                         String sname = cursor.getString(1);
                         int syear = cursor.getInt(2);
                         String gender = cursor.getString(3);
+                        String major = cursor.getString(4);
                         int score = cursor.getInt(5);
                         StudentVo vo = new StudentVo(sno, sname, syear, gender, major, score);
                         list.add(vo);
